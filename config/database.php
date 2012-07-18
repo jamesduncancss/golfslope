dbf1f29afc19f0308d055767879cb28d38721001
jamesduncancss <jameswduncan@gmail.com>
2012-07-18 12:27:30 -0300

    Update master

--- config/database.php
+++ config/database.php
@@ -50,8 +50,8 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'cakephp';
-$db['default']['password'] = '9ZSwGTqD9sRxV2V4';
-$db['default']['database'] = 'cakephp';
+$db['default']['password'] = '';
+$db['default']['database'] = '';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
